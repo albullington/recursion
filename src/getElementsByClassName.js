c092df372dd0e0a565b75003418b1538f8b55e1d
Amanda <albullington@gmail.com>
2018-02-22 17:06:28 -0800

    Finished stringify, still working on getElements.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,65 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var body = document.body; //returns the body of the current document
+    var classNameArray = [];
+
+    var addToClassNameArray = function(element) {
+      var classList = element.classList; //returns a live DOMTokenList (space separated token) collection of the class attributes of the element
+      if (classList !== undefined && classList.contains(className) === true) {
+        classNameArray.push(className);
+	  }
+    };
+
+    var checkNodesWithRecursiveLoop = function(element) { //check first children, then body
+      var children = element.childNodes;
+      if (children === undefined) {
+        getElementsByClassName()
+      }
+
+      
+      //for (var i = 0; i < children.length; i++) {
+        
+      //}
+    }
+
+    
+
+    //test nodes with a recursive function
+    //why? is document.body a node? why doesn't this test everything?
+	
+	//Returns an array-like object of all child elements which have all of the given class names. 
+    //need a base case, since recursion
+    //then, need a way to call getElementsByClassName(className) again
+
+	 
+    //why would I need this? need to specify that I'm searching within the body
+
+	 //returns live collection of the child nodes of given element, where first node is assigned index 0;
+	//do I need to check to see if element HAS childnodes? mdn
+
+    return classNameArray;
+
+    //can either call getElements on an element or the body, which will search everything
+
 };
+
+//document.body, element.childNodes, and element.classList
+
+/*
+ $('body').addClass('targetClassName');
+htmlStrings.forEach(function(htmlString) {
+  var $rootElement = $(htmlString);
+  $('body').append($rootElement);
+
+  var result = getElementsByClassName('targetClassName');
+  var expectedNodeList = document.getElementsByClassName('targetClassName');
+  var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+  var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+  expect(equality).to.equal(true);
+
+  $rootElement.remove();
+});
+$('body').removeClass('targetClassName');
+*/
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,76 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrayForArray = [];
+  var arrayForObject = [];
+
+  if (obj === null) {
+  	return 'null';
+  } else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+  	return '' + obj;
+  } else if (typeof obj === 'function' || typeof obj === 'undefined' || typeof obj === 'symbol' || typeof obj === 'null') {
+  	return 'null';
+  } else if (Array.isArray(obj)) {
+      obj.forEach(function(value) {
+        arrayForArray.push(stringifyJSON(value));
+      });
+      return '[' + arrayForArray.join(',') + ']';
+  } else if (typeof obj === 'object') {
+  	  Object.keys(obj).forEach(function(key) {
+  	  if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined' || typeof obj[key] === 'symbol') {
+  	    return '{}';
+  	  } else {
+  	    var keyString = '"' + key + '":';
+  	    var objProperty = stringifyJSON(obj[key]);
+  	  
+  	    arrayForObject.push(keyString + objProperty);
+  	  }
+  	  });
+  	 return '{' + arrayForObject + '}';
+  	}
 };
+
+
+/*
+Test cases checked!!
+
+DONE JSON.stringify({});                  // '{}'
+DONE JSON.stringify(true);                // 'true'
+DONE JSON.stringify('foo');               // '"foo"'
+DONE JSON.stringify([1, 'false', false]); // '[1,"false",false]'
+DONE JSON.stringify({ x: 5 });            // '{"x":5}'
+DONE JSON.stringify({ x: 5, y: 6 });
+// '{"x":5,"y":6}'
+DONE JSON.stringify({ x: [10, undefined, function(){}, Symbol('')] }); 
+// '{"x":[10,null,null,null]}' 
+
+DONE // Non-enumerable properties:
+JSON.stringify( Object.create(null, { x: { value: 'x', enumerable: false }, y: { value: 'y', enumerable: true } }) ); // '{"y":"y"}'
+//
+
+NEEDS WORK JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)) 
+// '"2006-01-02T15:04:05.000Z"'
+
+NEEDS WORK JSON.stringify([new Number(3), new String('false'), new Boolean(false)]);
+// '[3,"false",false]'
+
+DONE // Symbols:
+JSON.stringify({ x: undefined, y: Object, z: Symbol('') });
+// '{}'
+JSON.stringify({ [Symbol('foo')]: 'foo' });
+// '{}'
+JSON.stringify({ [Symbol.for('foo')]: 'foo' }, [Symbol.for('foo')]);
+// '{}'
+JSON.stringify({ [Symbol.for('foo')]: 'foo' }, function(k, v) {
+  if (typeof k === 'symbol') {
+    return 'a symbol';
+  }
+});
+// '{}'
+
+
+*/
